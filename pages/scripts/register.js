0c8886b2c5624bb7957abc9e33d5dd2eb9036502
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2026-01-24 10:36:06 +0100

    fix: fix minor bug with registration.

--- pages/scripts/register.js
+++ pages/scripts/register.js
@@ -21,7 +21,7 @@ document.addEventListener("input", (e) => {
           createRepeatPasswordValidation(
             password,
             repeatPassword,
-            validateRepeatPassword,
+            validateRepeatPassword
           ),
         ]);
       }
@@ -33,7 +33,7 @@ document.addEventListener("input", (e) => {
         createRepeatPasswordValidation(
           password,
           repeatPassword,
-          validateRepeatPassword,
+          validateRepeatPassword
         ),
       ]);
       break;
@@ -55,7 +55,7 @@ document.addEventListener("submit", async (e) => {
     createRepeatPasswordValidation(
       password,
       repeatPassword,
-      validateRepeatPassword,
+      validateRepeatPassword
     ),
   ];
   message.className = "";
@@ -79,6 +79,7 @@ document.addEventListener("submit", async (e) => {
       }),
     });
   } catch {
+    button.disabled = false;
     message.textContent = "Server error.";
   }
 
@@ -128,7 +129,7 @@ function createValidation(input, validationFunction) {
 function createRepeatPasswordValidation(
   passwordInput,
   repeatInput,
-  validationFunction,
+  validationFunction
 ) {
   return {
     input: repeatInput,
@@ -159,7 +160,7 @@ function validateEmail(email) {
 
   if (
     !email.match(
-      /(?:[a-z0-9!#$%&'*+\x2f=?^_`\x7b-\x7d~\x2d]+(?:\.[a-z0-9!#$%&'*+\x2f=?^_`\x7b-\x7d~\x2d]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9\x2d]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9\x2d]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9\x2d]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
+      /(?:[a-z0-9!#$%&'*+\x2f=?^_`\x7b-\x7d~\x2d]+(?:\.[a-z0-9!#$%&'*+\x2f=?^_`\x7b-\x7d~\x2d]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9\x2d]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9\x2d]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9\x2d]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
     )
   ) {
     return { ok: false, message: "Email is invalid." };
